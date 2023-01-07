f511cd83fa699c0aaa51041021b00e20eddc9812
HeriEspinosa <espinosa_reyes@hotmail.com>
2023-01-07 01:33:26 -0400

    modal mobile

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+
+const navarShow = document.querySelector(".navbar");
+const barraShow = document.querySelector(".menu");
+const links = document.querySelectorAll(".menu li a");
+const menuShow = document.querySelector(".barra__menu");
+const barraHidden = document.querySelector(".menu_show-hidden");
+
+console.log(links);
+
+menuShow.addEventListener("click", function(){
+    navarShow.classList.toggle("menu_show");
+});
+
+menuShow.addEventListener("click", function(){
+    barraShow.classList.toggle("menu_show");
+});
+
+menuShow.addEventListener("click", function(){
+    menuShow.classList.toggle("menu_show-hidden");
+});
+
+
+
+links.forEach((link) => {
+    link.addEventListener("click", function(){
+        barraShow.classList.toggle("menu_show");
+        navarShow.classList.toggle("menu_show");
+        menuShow.classList.toggle("menu_show-hidden");
+    });
+});
